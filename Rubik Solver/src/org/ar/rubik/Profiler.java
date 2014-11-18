5c07ef8e248fc99199a0408167906cc1f1f24e81
AndroidSteve <steven.punte@gmail.com>
2014-11-18 11:09:02 -0800

    Filled out lesson plan some.  Added min times to annotation.

--- Rubik Solver/src/org/ar/rubik/Profiler.java
+++ Rubik Solver/src/org/ar/rubik/Profiler.java
@@ -47,11 +47,13 @@ public class Profiler {
 	public enum Event { START, GREYSCALE, GAUSSIAN, EDGE, DILATION, CONTOUR, POLYGON, RHOMBUS, FACE, CONTROLLER, TOTAL };
 	
 	private Map<Event,Long> eventSet = new HashMap<Event,Long>(32);
-//	private Map<Event,Long> minEventSet = new HashMap<Event, Long>(32);
+	private static Map<Event,Long> minEventSet = new HashMap<Event, Long>(32);
 	
 	public void markTime(Event event) {
 		long time = System.currentTimeMillis();
 		eventSet.put(event, time);
+		if(minEventSet.containsKey(event) == false)
+			minEventSet.put(event, Long.MAX_VALUE);
 	}
 	
 	/**
@@ -63,8 +65,11 @@ public class Profiler {
 	 */
 	public Mat renderTimeConsumptionMetrics(Mat image, StateModel2 stateModel2) {
 
-    	Core.rectangle(image, new Point(0, 0), new Point(450, 720), Constants.ColorBlack, -1);
+    	Core.rectangle(image, new Point(0, 0), new Point(500, 720), Constants.ColorBlack, -1);
 		int index = 0;
+		
+
+		Core.putText(image, "Event    Time  Min", new Point(50, 50), Constants.FontFace, 2, Constants.ColorWhite, 2);
 				
 		renderAndIndex(Event.GREYSCALE,  Event.START,     image, index++);
 		renderAndIndex(Event.GAUSSIAN,   Event.GREYSCALE, image, index++);
@@ -84,11 +89,11 @@ public class Profiler {
 	/**
 	 * Render one line of time consumption
 	 * 
-	 * @param startEvent TODO
-	 * @param image TODO
-	 * @param index TODO
-	 * @param greyscale
-	 */
+     * @param endEvent
+     * @param startEvent
+     * @param image
+     * @param index
+     */
     private void renderAndIndex(Event endEvent, Event startEvent, Mat image, int index) {
     	if(eventSet.containsKey(endEvent) == false) {
     		Core.putText(image, endEvent.toString() + ": NA", new Point(50, 100 + 50 * index), Constants.FontFace, 2, Constants.ColorWhite, 2);
@@ -96,7 +101,15 @@ public class Profiler {
     	else {
     		long endTimeStamp = eventSet.get(endEvent);
     		long startTimeStamp = eventSet.get(startEvent);
-    		String string = String.format("%10s: %3d mS", endEvent.toString(), endTimeStamp - startTimeStamp);
+    		long elapsedTime = endTimeStamp - startTimeStamp;
+    		
+    		long minValue = minEventSet.get(endEvent);
+    		if(elapsedTime < minValue) {
+    			minValue = elapsedTime;
+    			minEventSet.put(endEvent, minValue);
+    		}
+
+			String string = String.format("%10s: %3dmS %3dmS", endEvent.toString(), elapsedTime, minValue);
     		Core.putText(image, string, new Point(50, 100 + 50 * index), Constants.FontFace, 2, Constants.ColorWhite, 2);
     	}
     }
